2b8f96ff129dd1ca6cb6c79830a37b10da8e24fc
Nikhil Gajjar <nikgajjar51@gmail.com>
2023-01-14 19:44:50 -0500

    Clean up for New Code

--- include/675E/driver-control.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "EZ-Template/drive/drive.hpp"
-extern Drive chassis;
-extern bool triple_shooter_toggle, drive_lock_toggle;
-extern int toggle_drive_lock();
-void flywheel_manual_control(void);
-void intake_manual_control(void);
-void expansion_manual_control(void);
-void drive_lock_control(void);
